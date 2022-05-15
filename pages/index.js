b6df54aaa9ab6c73ee62e59cf4f1131281371015
Georgy <georgy.mamardashvili@gmail.com>
2022-05-15 13:46:15 +0300

    added styling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import 'semantic-ui-css/semantic.min.css'
 
 class CampaignList extends Component {
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
         return { campaigns };
     }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map((address) => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true, 
+            };
+        });
+        return <Card.Group items={items}></Card.Group>
+    }
     
     render() {
-        return (<div>{this.props.campaigns[0]}</div>)
+        return (<div>
+            {this.renderCampaigns()}
+        </div>);
     };
 }
 
